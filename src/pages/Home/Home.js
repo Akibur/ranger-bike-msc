1efbc90bdb61ec262fc3137012af3bda62973604
Akib <akib3366@gmail.com>
2021-11-16 21:23:53 +0600

    Home about section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Banner from './Banner/Banner';
 import Products from '../../Components/Products/Products';
 import Reviews from '../../Components/Reviews/Reviews';
+import HomeAbout from './HomeAbout/HomeAbout';
 
 export default function Home() {
     return (
@@ -10,7 +11,11 @@ export default function Home() {
             <Banner></Banner>
             <h1 className="text-center my-8 text-5xl font-bold">Custom Bikes</h1>
             <Products></Products>
+            <h1 className="text-center my-8 text-5xl font-bold">About Us</h1>
+
+            <HomeAbout></HomeAbout>
             <h1 className="text-center my-8 text-5xl font-bold">Reviews</h1>
+
             <Reviews></Reviews>
 
         </>
