f45d1674a45e31e12b85827d8b1ec06d47e3c0b1
Akib <akib3366@gmail.com>
2021-11-16 12:42:55 +0600

    home added

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Banner from './Banner/Banner';
+import Products from '../../Components/Products/Products';
+import Reviews from '../../Components/Reviews/Reviews';
+
+export default function Home() {
+    return (
+        <>
+            <Banner></Banner>
+            <h1 className="text-center my-8 text-5xl font-bold">Custom Bikes</h1>
+            <Products></Products>
+            <h1 className="text-center my-8 text-5xl font-bold">Reviews</h1>
+            <Reviews></Reviews>
+
+        </>
+
+    );
+}
