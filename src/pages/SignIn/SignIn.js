f75f8878c8382df6a6a9355435ba941884dd0971
Akib <akib3366@gmail.com>
2021-11-25 02:02:31 +0600

    Google signin added

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { NavLink, useHistory, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import { useForm } from "react-hook-form";
+
 
 export default function SignIn() {
     const [loginData, setLoginData] = useState({});
@@ -8,18 +10,13 @@ export default function SignIn() {
 
     const location = useLocation();
     const history = useHistory();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    const handleOnChange = e => {
-        const field = e.target.name;
-        const value = e.target.value;
-        const newLoginData = { ...loginData };
-        newLoginData[field] = value;
-        setLoginData(newLoginData);
+
+    const onSubmit = data => {
+        loginUser(data.email, data.password, location, history);
     };
-    const handleLoginSubmit = e => {
-        loginUser(loginData.email, loginData.password, location, history);
-        e.preventDefault();
-    };
+
 
     const handleGoogleSignIn = () => {
         signInWithGoogle(location, history);
@@ -43,24 +40,33 @@ export default function SignIn() {
                     </div> : null}
 
                 {/* google auth */}
-                {/* <div className="flex gap-4 item-center">
-                    <button type="button" className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                <div className="flex gap-4 item-center">
+
+                    <button disable type="button" className="py-2 px-4 flex justify-center items-center  bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                         <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z">
                             </path>
                         </svg>
-                        Facebook
+                        <p>Facebook</p>
+                        <br />
+                        <p className="block text-xs text-center ">(Comming Soon)</p>
+
+
                     </button>
-                    <button type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+
+
+
+
+                    <button onClick={handleGoogleSignIn} type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                         <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z">
                             </path>
                         </svg>
                         Google
                     </button>
-                </div> */}
+                </div>
                 <div className="mt-8">
-                    <form onSubmit={handleLoginSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="flex flex-col mb-2">
                             <div className="flex relative ">
                                 <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
@@ -69,12 +75,13 @@ export default function SignIn() {
                                         </path>
                                     </svg>
                                 </span>
-                                <input type="text"
+                                <input {...register("email", { required: true })} type="text"
                                     className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Your email"
                                     name="email"
-                                    onChange={handleOnChange}
                                 />
                             </div>
+                            {errors.email && <span className="text-red-500">* Email is required</span>}
+
                         </div>
                         <div className="flex flex-col mb-6">
                             <div className="flex relative ">
@@ -85,18 +92,16 @@ export default function SignIn() {
                                     </svg>
                                 </span>
                                 <input
+                                    {...register("password", { required: true })}
                                     type="password"
                                     className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Your password"
                                     name="password"
-                                    onChange={handleOnChange}
                                 />
                             </div>
+                            {errors.password && <span className="text-red-500">* Password is required</span>}
+
                         </div>
-                        <div className="flex items-center justify-center mb-6 -mt-4">
-                            <a href="#" className="text-center inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white">
-                                Forgot Your Password?
-                            </a>
-                        </div>
+
                         <div className="flex w-full">
                             <button type="submit" className="py-2 px-8  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                                 <div className='flex justify-center'>
