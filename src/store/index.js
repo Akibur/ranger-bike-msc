f03195915d13b499b201a8ba27599595feb52205
Akib <akib3366@gmail.com>
2021-11-24 13:30:15 +0600

    redux refactor for allOrders

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsSlice from './products-slice';
+import ordersSlice from './orders-slice';
+
 
 const store = configureStore({
     reducer: {
-        products: productsSlice
+        products: productsSlice,
+        orders: ordersSlice
     },
 });
 
--- src/store/orders-slice.js
+++ src/store/orders-slice.js
@@ -1,10 +1,12 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 
 
 const initialState = {
     orders: [],
     order: {},
     loading: false,
+    statusUpdateLoading: false,
     orderError: {
         isError: false,
         message: ""
@@ -13,13 +15,17 @@ const initialState = {
 };
 
 
-
-export const getOrders = createAsyncThunk(
-    'orders/getOrders',
+export const getAllOrders = createAsyncThunk(
+    'orders/getAllOrders',
     async (thunkAPI) => {
-
         try {
-            const res = await fetch('https://sheltered-crag-02874.herokuapp.com/orders').then(
+            const res = await fetch('https://sheltered-crag-02874.herokuapp.com/orders',
+                {
+                    headers: {
+                        "authorization": `Bearer ${localStorage.getItem('idToken')}`
+                    }
+                }
+            ).then(
                 (data) => data.json()
             );
             return res;
@@ -37,7 +43,6 @@ export const createOrder = createAsyncThunk(
     async (order, thunkAPI) => {
 
         try {
-
             const res = await fetch(`https://sheltered-crag-02874.herokuapp.com/orders/`, {
                 method: 'POST',
                 headers: {
@@ -60,6 +65,32 @@ export const createOrder = createAsyncThunk(
 
     });
 
+export const updateOrder = createAsyncThunk(
+    'orders/updateOrder',
+    async (update, thunkAPI) => {
+        const { id, status } = update;
+        try {
+            const order = { status: status };
+            const res = await fetch(`https://sheltered-crag-02874.herokuapp.com/orders/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(order)
+            }).then(data => data.json());
+            console.log(res);
+            thunkAPI.dispatch(getAllOrders());
+            return res;
+        } catch (err) {
+            console.log("try catch err", err);
+            if (!err.response) {
+                throw err;
+            }
+            return thunkAPI.rejectWithValue(err.response.data);
+        }
+
+
+    });
 
 const ordersSlice = createSlice({
     name: 'orders',
@@ -75,14 +106,14 @@ const ordersSlice = createSlice({
     },
     extraReducers: {
         //GetOrders
-        [getOrders.pending]: (state) => {
-            state.loading = true;
+        [getAllOrders.pending]: (state) => {
+            if (state.orders.length < 1) state.loading = true;
         },
-        [getOrders.fulfilled]: (state, action) => {
+        [getAllOrders.fulfilled]: (state, action) => {
             state.loading = false;
             state.orders = action.payload;
         },
-        [getOrders.rejected]: (state, action) => {
+        [getAllOrders.rejected]: (state, action) => {
             state.loading = false;
             state.orderError.isError = true;
             state.orderError.message = action;
@@ -101,6 +132,23 @@ const ordersSlice = createSlice({
             state.orderError.isError = true;
             state.orderError.message = action.error.message;
         },
+
+        //UpdateOrder
+        [updateOrder.pending]: (state) => {
+            state.statusUpdateLoading = true;
+        },
+        [updateOrder.fulfilled]: (state, action) => {
+            state.statusUpdateLoading = false;
+            console.log(action.payload);
+
+            // state.orders = action.payload
+        },
+        [updateOrder.rejected]: (state, action) => {
+            state.statusUpdateLoading = false;
+            state.orderError.isError = true;
+            state.orderError.message = action.error.message;
+        },
+
 
     },
 });
